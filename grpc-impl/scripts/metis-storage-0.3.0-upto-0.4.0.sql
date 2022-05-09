add576e1b80f699185226e2803df1b78d35232cc
liushuyu1 <754053007@qq.com>
2022-05-09 15:11:06 +0800

    修改大字段长度

--- grpc-impl/scripts/metis-storage-0.3.0-upto-0.4.0.sql
+++ grpc-impl/scripts/metis-storage-0.3.0-upto-0.4.0.sql
@@ -36,7 +36,7 @@ drop table IF EXISTS meta_data_option_part;
 CREATE TABLE `meta_data_option_part` (
                                          `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
                                          `meta_data_id` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'metaDataId',
-                                         `meta_data_option_part` text COLLATE utf8mb4_unicode_ci COMMENT 'metaDataOption分片存储,顺序存储',
+                                         `meta_data_option_part` longtext COLLATE utf8mb4_unicode_ci COMMENT 'metaDataOption分片存储,顺序存储',
                                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
@@ -68,7 +68,7 @@ drop table IF EXISTS task_data_flow_option_part;
 CREATE TABLE `task_data_flow_option_part` (
                                               `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
                                               `task_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '关联的任务ID',
-                                              `data_flow_option_part` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '数据片段',
+                                              `data_flow_option_part` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '数据片段',
                                               PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='任务的数据流向策略的内容 (json字符串, 和 data_flow_policy_type 配套使用)';
 
@@ -76,7 +76,7 @@ drop table IF EXISTS task_data_option_part;
 CREATE TABLE `task_data_option_part` (
                                          `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
                                          `task_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '关联的任务ID',
-                                         `data_option_part` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '分片后的信息',
+                                         `data_option_part` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '分片后的信息',
                                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='任务的数据提供方选择数据策略的内容 (json字符串, 和 data_policy_type 配套使用)';
 
@@ -112,8 +112,8 @@ drop table IF EXISTS task_inner_algorithm_code_part;
 CREATE TABLE `task_inner_algorithm_code_part` (
                                                   `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
                                                   `task_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '关联的任务ID',
-                                                  `algorithm_code_part` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '内置算法代码 (使用内置算法, 在不使用算法市场前提下用)',
-                                                  `algorithm_code_extra_params_part` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '内置算法的额外超参 (使用内置算法, 内置算法的额外超参数 json 字符串, 内容可为""空字符串, 根据算法来)',
+                                                  `algorithm_code_part` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '内置算法代码 (使用内置算法, 在不使用算法市场前提下用)',
+                                                  `algorithm_code_extra_params_part` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '内置算法的额外超参 (使用内置算法, 内置算法的额外超参数 json 字符串, 内容可为""空字符串, 根据算法来)',
                                                   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='任务内置算法代码';
 
@@ -132,7 +132,7 @@ drop table IF EXISTS task_power_option_part;
 CREATE TABLE `task_power_option_part` (
                                           `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
                                           `task_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '关联的任务ID',
-                                          `power_option_part` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '数据片段',
+                                          `power_option_part` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '数据片段',
                                           PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='任务的算力提供方选择算力策略的内容 (json字符串, 和 power_policy_type 配套使用)';
 
@@ -222,6 +222,6 @@ DROP TABLE IF EXISTS `meta_data_auth`;
 CREATE TABLE `task_receiver_option_part` (
                                              `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
                                           `task_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '关联的任务ID',
-                                          `receiver_option_part` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '数据片段',
+                                          `receiver_option_part` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '数据片段',
                                           PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='任务的接收方选择策略的内容 (json字符串数组, 和 receiver_policy_types 配套使用, 使用数组的原因是 可以支持单个或者多个数目的策略)';
